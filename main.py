3ebdb66c4220b512eec9d13f4980986a52f9dac9
AxolotlGames <h@townsendwatkins.com>
2026-06-03 22:01:45 -0400

    Started to add stack methods

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import math
 
 # Graphics
-Rect(75,20,250,360,fill=None,border="black",borderWidth=4)
-for i in range(4):
-    Rect(85,30+35*i,230,30,fill="lightgrey")
+Rect(75,20,250,365,fill=None,border="black",borderWidth=4)
+for i in range(3):
+    Rect(85,30+35*i,230,30,fill=rgb(210,210,210))
+Rect(85,140,230,30,fill=rgb(180,180,180))
 
 # Buttons
 buttons = [[0 for _ in range(5)] for _ in range(5)]
@@ -14,12 +15,12 @@ labels = [['C',' ',' ',' ',' '],
           ['0','.',' ','/',' ']]
 for row in range(5):
     for col in range(5):
-        buttons[row][col] = Rect(100+col*40,175+row*40,35,35,fill="lightgrey")
+        buttons[row][col] = Rect(100+col*40,180+row*40,35,35,fill="lightgrey")
         labels[row][col] = Label(labels[row][col],117.5+col*40,192.5+row*40,size=18)
 
 clearAll = buttons[0][2]
 clearAll.width = 75
-clearAll.fill = "yellow"
+# clearAll.fill = "yellow"
 buttons[0][3].fill = None
 labels[0][1] = Label("AC",217,193,size=18)
 
@@ -56,12 +57,44 @@ for i in range(10):
     digits[i] = buttons[1+math.floor(i/3)][i%3]
 
 # Stack
-stack = []
-for i in range(4):
-    Label("0",303,150-i*35,size=18)
+stack = [0]
+isDecimal = False
+stackLabels = []
+stackLabels.append(Label("0",303,155,size=18))
+for i in range(3):
+    stackLabels.append(Label("0",303,115-i*35,size=18))
 
 def getNum(index):
     if (len(stack) > index):
         return stack[index]
     else:
-        return 0
+        pass # TODO: error message
+        
+def pop():
+    if (len(stack) > 0):
+        return stack.pop()
+    else:
+        pass # TODO: error message
+        
+def enter():
+    stack.insert(0, 0)
+    
+def swap():
+    if (len(stack) > 1):
+        temp = stack[0]
+        stack[0] = stack[1]
+        stack[1] = temp
+    else:
+        pass # TODO: error message
+
+def setNum(num):
+    if (len(stack) > 0):
+        stack[0] = num
+    else:
+        stack.push(num)
+        
+def clear():
+    if (len(stack) > 0):
+        setNum(0)
+    else:
+        pass # TODO: error message
